d1453e8ddd9b5aa80076fa3d27e4b5e0d833edfb
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-08-23 00:36:00 +0300

    handle return -1

--- _printf.c
+++ _printf.c
@@ -14,6 +14,10 @@ int _printf(const char *format, ...)
 
 	counter = 0;
 	va_start(args, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 	while (*format != '\0')
 	{
 		if (*format == '%')
