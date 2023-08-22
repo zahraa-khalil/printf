d52d3eda307cd929e60d323075cc354aaf976f79
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-08-23 02:09:10 +0300

    update betty

--- puts_recursion.c
+++ puts_recursion.c
@@ -8,15 +8,15 @@
 
 int _puts_recursion(char *s)
 {
-    if (*s)
-    {
-        _putchar(*s);
-        _puts_recursion(s + 1);
-    }
-    else
-    {
-        return 0;
-    }
-    return 1;
-}
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		return (0);
+	}
 
+	return (1);
+}
