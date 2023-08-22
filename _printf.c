df9f403330f198b6e827575531bddf610f45210c
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-08-22 23:12:31 +0300

    handle case s

--- _printf.c
+++ _printf.c
@@ -8,43 +8,42 @@
 
 int _printf(const char *format, ...)
 {
-	/*list of variadic func arguments[args] */
 	va_list args;
-	int counter, first_percent, num;
+	int counter, num;
+	char *str;
 
 	counter = 0;
-	first_percent = 0;
-	/*access to variadic function arguments. */
 	va_start(args, format);
-
 	while (*format != '\0')
 	{
 		if (*format == '%')
 		{
-			first_percent++;
 			switch (*(format + 1))
 			{
-			case 'd':
-			case 'i':
-				num = va_arg(args, int); /*** Retrieve the integer argument ***/
-				print_number(num);
-				format += 2;
-				break;
-			case 'c':
-				num = va_arg(args, int); /*** Retrieve the integer argument ***/
-				_putchar(num);
-				format += 2;
-				break;
-			default:
-				break;
+				case 'd':
+				case 'i':
+					num = va_arg(args, int);
+					print_number(num); /***Retrieve the integer argument ***/
+					break;
+				case 'c':
+					num = va_arg(args, int);
+					_putchar(num);
+					break;
+				case 's':
+
+					str = va_arg(args, char *);
+					_puts_recursion(str);
+					break;
+				default:
+					break;
 			}
+			format += 2;
 			counter++;
 		}
 		_putchar(*format);
 		counter++;
 		format++;
 	}
-	/*This ends the traversal of the variadic function arguments. */
 	va_end(args);
 	return (counter);
 }
--- puts_recursion.c
+++ puts_recursion.c
@@ -10,12 +10,9 @@ void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		_putchar((*s) + '0');
+		_putchar(*s);
 		s++;
 		_puts_recursion(s);
 	}
-	else
-	{
-		_putchar('\n');
-	}
+
 }
